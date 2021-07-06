cac89ebbda2de3e1d9b3cd77b360f522aacd69dd
AurumByte <80045511+AurumByte@users.noreply.github.com>
2021-07-06 07:01:11 +0000

    small updates

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import discord
 import os
 from keep_alive import keep_alive
+import commands.userinfo
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
 
-    if message.content.startswith('~hello'):
-        await message.channel.send('Hello! I\'m AurumBot!' )
+    print(message)
+    if not message.content.startswith("~") and not message.content.startswith(
+            "~~"):
+        return
+    
+    command = message.content.split()
+
+    if command[0] == "~hello":
+        await message.channel.send('Hello! I\'m AurumBot!')
+    else:
+        await message.channel.send(f"Invalid command! Command \"{command[0][1:]}\" does not exist!")
 
 keep_alive()
 client.run(os.environ['TOKEN'])
